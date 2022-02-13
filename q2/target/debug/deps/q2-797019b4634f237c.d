788841f8ba11e0ed6fb9ae715d3bb4f99a94dee4
cameron <cameron.studdstreet@gmail.com>
2022-02-13 00:46:43 +0000

    q1 done

--- q2/target/debug/deps/q2-797019b4634f237c.d
+++
@@ -1,7 +0,0 @@
-/home/cameron/projects/project_euler/q2/target/debug/deps/q2-797019b4634f237c.rmeta: src/main.rs
-
-/home/cameron/projects/project_euler/q2/target/debug/deps/q2-797019b4634f237c.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=
